5810756e6a78f54112e6af56e60c06a0d365364b
ahyben <andrej.hyben@cancom.de>
2026-07-14 23:12:17 +0200

    v1.0.0_fix

--- csdm/__init__.py
+++ csdm/__init__.py
@@ -5,7 +5,7 @@ class CSDMConfig(PluginConfig):
     name = 'csdm'
     verbose_name = 'CSDM'
     description = 'Business and Technical Service Offerings (CSDM) for NetBox'
-    version = '0.9.1'
+    version = '1.0.0'
     author = 'ThaseG'
     base_url = 'csdm'
     min_version = '4.6.0'
