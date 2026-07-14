ff12ef9be1ebf7c0086615445537881cf51002b5
ahyben <andrej.hyben@cancom.de>
2026-07-14 10:48:14 +0200

    Updated

---
+++ docker/configuration/plugins.py
@@ -0,0 +1,24 @@
+PLUGINS = [
+    "netbox_custom_objects",
+    "netbox_topology_views",
+    "netbox_lifecycle",
+    "netbox_secrets",
+]
+
+PLUGINS_CONFIG = {
+    "netbox_topology_views": {
+        "allow_coordinates_saving": True,
+        "always_save_coordinates": False,
+    },
+    "netbox_lifecycle": {
+        "lifecycle_card_position": "right_page",
+        "contract_card_position": "right_page",
+    },
+    "netbox_secrets": {
+        # Objects that can have secrets attached. Extend as needed.
+        "apps": [
+            "dcim.device",
+            "virtualization.virtualmachine",
+        ],
+    },
+}
