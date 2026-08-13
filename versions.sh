01695f18525aa5e7abb7816200d4f0bc34f44823
ahyben <andrej.hyben@cancom.de>
2026-08-13 15:38:31 +0200

    Tests changed

--- versions.sh
+++ versions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
 # netboxcommunity/netbox image tag: "<netbox-version>-<netbox-docker-build>"
+
 export NETBOX_VERSION="v4.6.5-5.0.2"
 export SERVICE_SPECIFICATION_PLUGIN_VERSION="1.0.0"
